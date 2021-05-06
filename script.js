e9761c7515a5998b8ef453c5796d8998affda32a
Thuanny <thucatm@gmail.com>
2021-05-05 22:18:00 -0300

    função scroll suave link interno

--- script.js
+++ script.js
@@ -38,5 +38,33 @@ function initAccordion() {
         });
     }    
 }
-initAccordion()
+initAccordion();
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);    
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    
+        //Forma alternativa 
+        //const topo = section.offsetTop;
+    
+        //window.scrollTo({
+            //top: 1000,
+            //behavior: 'smooth',
+        // });
+    }
+    
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    })
+}
+initScrollSuave();
+
 
